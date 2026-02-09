0fb0d50c3a866dff27e98bb0c950ffee8b0dbd17
Marina Lendt <marinalendt@gmail.com>
2026-02-09 14:06:24 +0100

    adding listendpoints from express

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import listEndpoints from "express-list-endpoints";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/final-project";
 mongoose.connect(mongoUrl);
@@ -13,7 +14,12 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  const endpoints = listEndpoints(app);
+
+  res.json([{
+    message: "Welcome to the Balans API",
+    endpoints: endpoints,
+  }])
 });
 
 // Start the server

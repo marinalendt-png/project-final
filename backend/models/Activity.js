db03428edeff9d2aed84a56de8e5bc613357d0a7
Marina Lendt <marinalendt@gmail.com>
2026-02-10 12:19:38 +0100

    adding model for activity and dailyplan

---
+++ backend/models/Activity.js
@@ -0,0 +1,18 @@
+import { Schema, model } from "mongoose";
+
+const activitySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  energyImpact: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+})
+
+export const Activity = model("activity", activitySchema);
---
+++ backend/models/DailyPlan.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+const dailyPlanSchema = new Schema({
+  // One unique id-person that points to the User model. The owner of the dailyplan.
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: () => new Date(),
+  },
+  startingEnergy: {
+    type: Number,
+  },
+  // Points to one specific activity. 
+  activities: {
+    type: Schema.Types.ObjectId,
+    ref: "activity",
+  },
+  currentEnergy: {
+    type: Number,
+  }
+})
+
+export const dailyPlan = model("dailyPlan", dailyPlanSchema); 

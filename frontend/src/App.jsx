de7de881fbb7c13bdcbacf2093436a0e206ebc2d
Marina Lendt <marinalendt@gmail.com>
2026-02-18 08:55:36 +0100

    adding routes for about and tips

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import { Register } from "./pages/Register";
 import { DailyPlan } from "./pages/DailyPlan";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 import { GlobalStyles } from "./styles/GlobalStyle";
+import { About } from "./pages/About";
+import { Tips } from "./pages/Tips";
 
 export const App = () => {
   return (
@@ -12,6 +14,23 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/about"
+          element={
+            <ProtectedRoute>
+              <About />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/tips"
+          element={
+            <ProtectedRoute>
+              <Tips />
+            </ProtectedRoute>
+          }
+        />
         <Route
           path="/daily"
           element={

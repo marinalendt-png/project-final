23be802d5cbbfaa283f771ea886a5c47316d2cc5
Marina Lendt <marinalendt@gmail.com>
2026-02-18 11:35:03 +0100

    adding endpoints to dailyplan, delete post and patch

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,5 +1,6 @@
-
 export const BASE_URL = "http://localhost:8080";
+
+// ======= ACTIVITIES ======= //
 
 // GET - fetches activities from the database
 export const fetchActivities = async () => {
@@ -19,7 +20,7 @@ export const fetchActivities = async () => {
   }
 };
 
-// POST
+// Create activity
 export const createActivity = async (activity) => {
   try {
     const accessToken = localStorage.getItem("accessToken");
@@ -39,7 +40,7 @@ export const createActivity = async (activity) => {
   }
 };
 
-// PATCH
+// Updates activity 
 export const patchActivities = async (activityId, updates) => {
   try {
     const accessToken = localStorage.getItem("accessToken");
@@ -59,8 +60,7 @@ export const patchActivities = async (activityId, updates) => {
   }
 };
 
-// DELETE
-
+// Delete activity 
 export const deleteActivities = async (activityId) => {
   try {
     const accessToken = localStorage.getItem("accessToken");
@@ -78,3 +78,67 @@ export const deleteActivities = async (activityId) => {
     throw error
   }
 };
+
+// ======= DAILYPLAN ======= //
+
+// Create a new dailyplan
+export const createDailyPlan = async (plan) => {
+  try {
+    const accessToken = localStorage.getItem("accessToken");
+    const res = await fetch(`${BASE_URL}/dailyplan`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": accessToken
+      },
+      body: JSON.stringify(plan)
+    });
+    if (!res.ok) throw new Error(`HTTP ${res.status}`);
+    return res.json();
+  } catch (error) {
+    console.error("posting dailyplan failed:", error)
+    throw error;
+  }
+};
+
+// Updates activity 
+export const patchDailyPlan = async (dailyPlanId, updates) => {
+  try {
+    const accessToken = localStorage.getItem("accessToken");
+    const res = await fetch(`${BASE_URL}/dailyplan/${dailyPlanId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": accessToken
+      },
+      body: JSON.stringify(updates),
+    });
+    if (!res.ok) throw new Error(`HTTP ${res.status}`);
+    return res.json();
+  } catch (error) {
+    console.error("updating dailyplan failed:", error);
+    throw error;
+  }
+};
+
+// Delete dailyplan
+export const deleteDailyPlan = async (dailyPlanId) => {
+  try {
+    const accessToken = localStorage.getItem("accessToken");
+    const res = await fetch(`${BASE_URL}/dailyplan/${dailyPlanId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": accessToken
+      }
+    });
+    if (!res.ok) throw new Error(`HTTP ${res.status}`);
+    return res.json();
+  } catch (error) {
+    console.error("deleting dailyplan failed:", error)
+    throw error
+  }
+};
+
+
+

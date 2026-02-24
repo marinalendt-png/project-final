18144ae292c1a9acf6d0a789d64a66ab5979c6ac
Marina Lendt <marinalendt@gmail.com>
2026-02-24 09:24:57 +0100

    fixes for better lighthouse-score

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,54 +1,61 @@
 import { BrowserRouter, Routes, Route } from "react-router";
+import { lazy, Suspense } from "react"; //för att få bättre prestande i Lighthouse, koden laddas bara när anv navigerar dit. 
 import { Home } from "./pages/Home";
 import { Register } from "./pages/Register";
 import { DailyPlan } from "./pages/DailyPlan";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 import { GlobalStyles } from "./styles/GlobalStyle";
-import { About } from "./pages/About";
 import { Tips } from "./pages/Tips";
-import { History } from "./pages/History";
+
+
+const About = lazy(() => import("./pages/About").then(m => ({ default: m.About })));
+const History = lazy(() => import("./pages/History").then(m => ({ default: m.History })));
 
 export const App = () => {
   return (
     <BrowserRouter>
       <GlobalStyles />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
+      <main>
+        <Suspense fallback={<div>Laddar...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
 
-        <Route
-          path="/about"
-          element={
-            <ProtectedRoute>
-              <About />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/tips"
-          element={
-            <ProtectedRoute>
-              <Tips />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/daily"
-          element={
-            <ProtectedRoute>
-              <DailyPlan />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/history"
-          element={
-            <ProtectedRoute>
-              <History />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+            <Route
+              path="/about"
+              element={
+                <ProtectedRoute>
+                  <About />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/tips"
+              element={
+                <ProtectedRoute>
+                  <Tips />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/daily"
+              element={
+                <ProtectedRoute>
+                  <DailyPlan />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/history"
+              element={
+                <ProtectedRoute>
+                  <History />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </Suspense>
+      </main>
+    </BrowserRouter >
   );
 };
